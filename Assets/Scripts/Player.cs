604791adac6c7c8f280bcde35b599b2175d134fd
Andrej <glazba@gmail.com>
2020-11-03 23:20:07 +0100

    movement boundaries tied to Viewport

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,23 +7,42 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float _moveSpeed = 1f;
 
+    private float _xMinViewPos = 0.07f;
+    private float _xMaxViewPos = 0.93f;
+    private float _xMinWorldPos;
+    private float _xMaxWorldPos;
+
+    private float _yMinViewPos = 0.03f;
+    private float _yMaxViewPos = 0.97f;
+    private float _yMinWorldPos;
+    private float _yMaxWorldPos;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetUpMoveBoundaries();
     }
 
-    // Update is called once per frame
-    void Update()
+	private void SetUpMoveBoundaries() {
+		Camera _gameCamera = Camera.main;
+        _xMinWorldPos = _gameCamera.ViewportToWorldPoint(new Vector3(_xMinViewPos, 0, 0)).x;
+		_xMaxWorldPos = _gameCamera.ViewportToWorldPoint(new Vector3(_xMaxViewPos, 0, 0)).x;
+        _yMinWorldPos = _gameCamera.ViewportToWorldPoint(new Vector3(0, _yMinViewPos, 0)).y;
+        _yMaxWorldPos = _gameCamera.ViewportToWorldPoint(new Vector3(0, _yMaxViewPos, 0)).y;
+    }
+
+	// Update is called once per frame
+	void Update()
     {
         Move();
     }
 
     private void Move() {
-        var _deltaX = Input.GetAxis("Horizontal");
-        var _posX = transform.position.x + _deltaX * Time.deltaTime * _moveSpeed;
-        var _deltaY = Input.GetAxis("Vertical");
-        var _posY = transform.position.y + _deltaY * Time.deltaTime * _moveSpeed;
-        transform.position = new Vector2(_posX, _posY);
+        var _deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * _moveSpeed;
+        var _deltaY = Input.GetAxis("Vertical") * Time.deltaTime * _moveSpeed;
+
+        var _newXPos = Mathf.Clamp (transform.position.x + _deltaX, _xMinWorldPos, _xMaxWorldPos);
+        var _newYPos = Mathf.Clamp (transform.position.y + _deltaY, _yMinWorldPos, _yMaxWorldPos);
+        transform.position = new Vector2(_newXPos, _newYPos);
     }
 }
