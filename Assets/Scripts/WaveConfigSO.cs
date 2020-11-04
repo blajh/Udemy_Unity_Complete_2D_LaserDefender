3f525d696c8b0a4cf780dc3300d8913f1e61e93f
Andrej <glazba@gmail.com>
2020-11-05 00:04:12 +0100

    defined Wave SO with private variables and public Get methods

---
+++ Assets/Scripts/WaveConfigSO.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (menuName = "EnemyWaveConfigSO")]
+public class WaveConfigSO : ScriptableObject
+{
+    [SerializeField] private GameObject _enemyPrefab;
+	[SerializeField] private GameObject _pathPrefab;
+	[SerializeField] private float _spawnTime = 0.5f;
+	[SerializeField] private float _spawnTimeOffset = 0.3f;
+	[SerializeField] private float _moveSpeed = 2f;
+	[SerializeField] private int _enemyCount = 5;
+
+	public GameObject GetEnemyPrefab() { return _enemyPrefab; }
+	public GameObject GetPathPrefab() { return _pathPrefab; }
+	public float GetSpawnTime() { return _spawnTime; }
+	public float GetSpawnTimeOffset() { return _spawnTimeOffset; }
+	public float GetMoveSpeed() { return _moveSpeed; }
+	public int GetEnemyCount() { return _enemyCount; }
+
+}
