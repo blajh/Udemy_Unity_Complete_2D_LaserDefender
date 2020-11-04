78adf1bdfc97c6118351764440eb5c0b5c116dc3
Andrej <glazba@gmail.com>
2020-11-04 23:49:47 +0100

    waypoint movement

--- Assets/Scripts/EnemyPathing.cs
+++ Assets/Scripts/EnemyPathing.cs
@@ -5,4 +5,29 @@ using UnityEngine;
 public class EnemyPathing : MonoBehaviour
 {
 	[SerializeField] private List<Transform> _waypoints;
+	[SerializeField] float _moveSpeed = 2f;
+	[SerializeField] private int _waypointIndex = 0;
+
+	private void Start() {
+		transform.position = _waypoints[_waypointIndex].transform.position;
+	}
+
+	private void Update() {
+		MoveAlongWaypoints();
+	}
+
+	private void MoveAlongWaypoints() {
+		if (_waypointIndex <= _waypoints.Count - 1) {
+			var _targetPosition = _waypoints[_waypointIndex].transform.position;
+			var _movementThisFrame = _moveSpeed * Time.deltaTime;
+			transform.position = Vector2.MoveTowards(transform.position, _targetPosition, _movementThisFrame);
+			if (transform.position == _targetPosition) {
+				_waypointIndex++;
+			}
+		}
+		else {
+			Destroy(gameObject);
+		}
+	}
 }
+ 
