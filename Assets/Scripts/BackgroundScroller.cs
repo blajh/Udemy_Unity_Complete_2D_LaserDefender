c590dec9d3b9d8e0a2133ce0e3346578d27a35b4
Andrej <glazba@gmail.com>
2020-11-07 22:17:53 +0100

    quad texture offset to animate material background

---
+++ Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    [SerializeField] private float _scrollSpeed = 0.1f;
+    private Material _material;
+    private Vector2 _offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _material = GetComponent<Renderer>().material;
+        _offset = new Vector2(0f, _scrollSpeed );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _material.mainTextureOffset += _offset * Time.deltaTime;
+    }
+}
