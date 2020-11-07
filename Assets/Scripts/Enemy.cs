80790e3035c1c040ea9c86a1fcad87bd9844f8ab
Andrej <glazba@gmail.com>
2020-11-07 21:14:46 +0100

    Make Enemies Shoot

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,7 +5,35 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-	[SerializeField] private float health = 100;
+	[SerializeField] private float _health = 100;
+	[SerializeField] private float _shotCounter;
+	[SerializeField] private float _minTimeBetweenShots = 0.2f;
+	[SerializeField] private float _maxTimeBetweenShots = 3f;
+	[SerializeField] private GameObject _laserPrefab;
+	[SerializeField] private float _projectileSpeed = 10f;
+
+	private void Start() {
+		RandomiseShotDuration();
+	}
+
+	private void Update() {
+		CountDownAndShoot();
+	}
+
+	private void CountDownAndShoot() {
+		_shotCounter -= Time.deltaTime;
+		if (_shotCounter <= 0) {
+			Fire();
+			RandomiseShotDuration();
+		}
+	}
+
+	private void Fire() {
+		GameObject _laser = Instantiate
+			(_laserPrefab, transform.position, Quaternion.identity)
+			as GameObject;
+		_laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, - _projectileSpeed);
+	}
 
 	private void OnTriggerEnter2D(Collider2D other) {
 		DamageDealer _damageDealer = other.gameObject.GetComponent<DamageDealer>();
@@ -13,10 +41,14 @@ public class Enemy : MonoBehaviour
 	}
 
 	private void ProcessHit(DamageDealer _damageDealer) {
-		health -= _damageDealer.GetDamage();
+		_health -= _damageDealer.GetDamage();
 		_damageDealer.Hit();
-		if (health <= 0) {
+		if (_health <= 0) {
 			Destroy(gameObject);
 		}
+	}
+
+	private void RandomiseShotDuration() {
+		_shotCounter = UnityEngine.Random.Range(_minTimeBetweenShots, _maxTimeBetweenShots);
 	}
 }
