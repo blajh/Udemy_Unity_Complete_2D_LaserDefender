046870f4672e52a6029cf49cebcc2274f5d8fba5
Andrej <glazba@gmail.com>
2020-11-08 23:18:05 +0100

    sorting and spinning

---
+++ Assets/Scripts/Spinner.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinner : MonoBehaviour
+{
+
+    void Update()
+    {
+        transform.Rotate(0f, 0f, 15f * Time.deltaTime);
+    }
+}
